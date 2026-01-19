c32e9f0c51f78fd100b757f66d8debcebddb6117
Pantelis <pantelos1999@gmail.com>
2026-01-19 23:09:27 +0200

    database credentials read from settings file .env

--- endpoints/helpers/connectMySQL.js
+++ endpoints/helpers/connectMySQL.js
@@ -1,11 +1,13 @@
 const mysql = require('mysql2/promise');
+const dotenv = require('dotenv');
 
+dotenv.config();
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "1kalampangala^%69**1209",
-  database: "improvementdares",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
@@ -14,5 +16,5 @@ const pool = mysql.createPool({
 if (pool) {
   console.log("MySQL Pool connected successfully.");
 }
-  
+
 module.exports=pool;
